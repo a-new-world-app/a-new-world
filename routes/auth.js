e36bcb00ee92645ccd77f74ad590d51b88ca6783
Ian MacLeod <ian.a.macleod@gmail.com>
2018-05-24 13:26:35 -0700

    Add route to get current user's info

--- routes/auth.js
+++ routes/auth.js
@@ -17,8 +17,11 @@ module.exports = app => {
     passport.authenticate("google"),
     async (req, res) => {
       const user = await User.findById(req.user._id);
-      crypto.randomBytes(48, function (err, buffer) {
-        var token = buffer.toString('base64').replace(/\//g, '_').replace(/\+/g, '-');
+      crypto.randomBytes(48, function(err, buffer) {
+        var token = buffer
+          .toString("base64")
+          .replace(/\//g, "_")
+          .replace(/\+/g, "-");
         user.sessionToken = token;
         user.save();
         res.redirect(`a-new-world://login?token=${user.sessionToken}`);
@@ -31,7 +34,10 @@ module.exports = app => {
     res.json("placeholder");
   });
 
-  app.get("/api/current_user", (req, res) => {
-    res.json(req.user);
+  app.get("/api/current_user", async (req, res) => {
+    const sessionToken = req.get("Authorization").match(/Bearer (.+)/);
+    const user = await User.findOne({ sessionToken });
+    console.log(JSON.stringify({ sessionToken: user.sessionToken }));
+    res.json(JSON.stringify({ sessionToken: user.sessionToken }));
   });
 };
--- routes/path.js
+++ routes/path.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 
-const Path = mongoose.model('paths');
-const User = mongoose.model('users');
+const Path = mongoose.model("paths");
+const User = mongoose.model("users");
 
 module.exports = app => {
-  app.post('/api/paths', async (req, res) => {
+  app.post("/api/paths", async (req, res) => {
     const path = new Path(req.body.path);
     const sessionToken = req.get("Authorization").match(/Bearer (.+)/);
-    const user = await User.findOne({sessionToken});
+    const user = await User.findOne({ sessionToken });
     path.userId = user.id;
     await path.save();
     res.json("things happened");
   });
-}
+};
