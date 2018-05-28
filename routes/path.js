0a86f3e6c1b1ccaa8c1bb31715e388f9fc4f7795
Ian MacLeod <ian.a.macleod@gmail.com>
2018-05-28 16:26:59 -0700

    Add path to upload images to cloudinary

--- routes/path.js
+++ routes/path.js
@@ -1,19 +1,35 @@
 const mongoose = require("mongoose");
+const fs = require("fs");
+var cloudinary = require("cloudinary");
 
 const Path = mongoose.model("paths");
 const User = mongoose.model("users");
 
 module.exports = app => {
   app.post("/api/paths", async (req, res) => {
-    const path = new Path(req.body.path);
     const auth = req.get("Authorization");
     if (!auth) {
       return res.status(403).json("Authorization required");
     }
     const sessionToken = auth.match(/Bearer (.+)/);
     const user = await User.findOne({ sessionToken });
+    const path = new Path(req.body.path);
     path.userId = user.id;
     await path.save();
-    res.json("things happened");
+    res.json(path);
+  });
+
+  app.post("/api/paths/images", async (req, res) => {
+    stream = cloudinary.uploader.upload_stream(
+      function(result) {
+        console.log(result);
+        res.json({ url: result.url, public_id: result.public_id });
+      },
+      { public_id: req.body.title }
+    );
+    fs
+      .createReadStream(req.files.image.path, { encoding: "binary" })
+      .on("data", stream.write)
+      .on("end", stream.end);
   });
 };
