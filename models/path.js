bee62e430075a156cda10080c262ea99699f7dc3
Ian MacLeod <ian.a.macleod@gmail.com>
2018-05-28 18:03:31 -0700

    Add route to get all completed paths

--- models/path.js
+++ models/path.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 const pathSchema = new Schema({
   userId: { type: Schema.Types.ObjectId, ref: "users", required: true },
   pathData: { type: Object, required: true },
-  finished: { type: Date }
+  completedAt: { type: Date }
 });
 
 mongoose.model("paths", pathSchema);
--- routes/path.js
+++ routes/path.js
@@ -21,7 +21,7 @@ module.exports = app => {
       const path = await Path.findById(user.activePathId);
       return res.json(path);
     }
-    res.json("no active path");
+    res.json(null);
   });
 
   app.post("/api/paths", async (req, res) => {
@@ -54,16 +54,16 @@ module.exports = app => {
       return res.status(403).json("Authorization required");
     }
 
-    const path = Path.findById(id);
+    const path = Path.findById(req.params.id);
     if (path.userId !== user.id) {
       return res.status(403).json("Authorization required");
     }
     path.pathData = req.body.path.pathData;
-    await path.save;
+    await path.save();
     res.json(path);
   });
 
-  app.post("/api/paths/finish", async (req, res) => {
+  app.post("/api/paths/complete", async (req, res) => {
     const auth = req.get("Authorization");
     if (!auth) {
       return res.status(403).json("Authorization required");
@@ -74,9 +74,11 @@ module.exports = app => {
       return res.status(403).json("Authorization required");
     }
 
+    path.completedAt = new Date();
+    path.save();
     user.activePathId = null;
     await user.save();
-    res.json(user)
+    res.json(user);
   });
 
   app.post("/api/paths/images", (req, res) => {
@@ -91,5 +93,10 @@ module.exports = app => {
       .createReadStream(req.files.image.path, { encoding: "binary" })
       .on("data", stream.write)
       .on("end", stream.end);
+  });
+
+  app.get("/api/completed_paths", async (req, res) => {
+    const completedPaths = await Path.find({ completedAt: { $exists: true } });
+    res.json(completedPaths);
   });
 };
